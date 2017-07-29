5393dfa337b9c7f84af43557fef83b452c348f6c
Clayton Pabst <claytonpabst@gmail.com>
2017-07-29 00:41:25 -0600

    Have first db call set up but its not working

--- server/index.js
+++ server/index.js
@@ -2,25 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 var massive = require('massive');
-var passport = require('passport');
+// var passport = require('passport');
 var session = require('express-session');
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var config = require('../config.js');
 
 const app = module.exports = express();
 
 app.use(bodyParser.json());
-app.use(session({
-  secret: config.secret,
-    resave: true,
-    saveUninitialized: false,
-    cookie:{
-      maxAge: (1000*60*60*24*14) //this is 14 days
-    }
-}))
+// app.use(session({
+//   secret: config.secret,
+//     resave: true,
+//     saveUninitialized: false,
+//     cookie:{
+//       maxAge: (1000*60*60*24*14) //this is 14 days
+//     }
+// }))
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 massive({
   host: config.host,
@@ -39,39 +39,47 @@ var userController = require("./userController.js");
 
 /////////////Oauth functions
 
-passport.use(new GoogleStrategy({
-  clientID: config.auth0.clientID,
-  clientSecret: config.auth0.clientSecret,
-  callbackURL: '/auth/callback'
-  },
-  userController.findById
+// passport.use(new GoogleStrategy({
+//   clientID: config.auth0.clientID,
+//   clientSecret: config.auth0.clientSecret,
+//   callbackURL: '/auth/callback'
+//   },
+//   userController.findById
+// ));
 
-));
-passport.serializeUser(function(user,done){
-  console.log('serializing',user);
-  done(null,user);
-});
-passport.deserializeUser(function(id,done){
-  db.find_by_id([id],function(err,user){
-    done(err,user);
-  });
-});
+// passport.serializeUser(function(user,done){
+//   console.log('serializing',user);
+//   done(null,user);
+// });
 
-///////Oauth endpoints
-app.get('/getuserinfo',userController.getUserInfo);
+// passport.deserializeUser(function(id,done){
+//   db.find_by_id([id],function(err,user){
+//     done(err,user);
+//   });
+// });
 
-app.get('/logout',function(req,res){
-  req.session.destroy(function(err,data){
-  });
-});
+///////Oauth endpoints ->
+// app.get('/getuserinfo',userController.getUserInfo);
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/calendar'] }));
+// app.get('/logout',function(req,res){
+//   req.session.destroy(function(err,data){
+//   });
+// });
 
-app.get('/auth/callback', passport.authenticate('google', { failureRedirect: '/LoginPage' }), function (req, res) {
-    res.redirect('/');
-});
+// app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/calendar'] }));
 
-//////////Other endpoints for the front end
+// app.get('/auth/callback', passport.authenticate('google', { failureRedirect: '/LoginPage' }), function (req, res) {
+//     res.redirect('/');
+// });
 
+//////////Other endpoints for the front end ->
+app.get('/api/Members', function(req, res){
+  db.getAllMembers()
+  .then( allMembers => {
+    return res.status(200).send( allMembers )
+  })
+})
 
-app.listen(config.port, console.log("you are now connected on " + config.port));
+const port = 3000;
+// app.listen(config.port, console.log("you are now connected on " + config.port));
+app.listen(port, console.log(`you are now connected on port ${port}`));
--- src/components/Teller/Teller.js
+++ src/components/Teller/Teller.js
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import './Teller.css';
 
 class Teller extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            allMembers: []
+        }
+
+        this.getAllMembers = this.getAllMembers.bind(this);
+
+    }
+
+    getAllMembers(){
+        axios.get('/api/Members')
+        .then( res => {
+            console.log(res)
+            this.setState({
+                allMembers: res.data
+            })
+        })
+    }
 
     render() {
         return (
-            <div>       
+            <div>
+                {this.state.allMembers}       
                 This is the Teller's Page
+                <button onClick={ this.getAllMembers }>Get All Members</button>
             </div>
         );
     }
